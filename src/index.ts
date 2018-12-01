32d197535bd268f27e650cfda1f7891168896f82
Peter Takacs <peter.takacs@koodilab.com>
2018-12-01 22:13:41 +0100

    Added the resources

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
-import { app } from "electron";
+import { app, BrowserWindow } from "electron";
+import path from "path";
 
-app.on("ready", () => {
-    // tslint:disable-next-line:no-console
-    console.log("App is ready");
+let mainWindow: BrowserWindow | null;
+
+const createMainWindow = () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.on("closed", () => mainWindow = null);
+};
+
+app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
 });
