ecc02a47755110e2b60263e8a6096d4cf285e37e
Peter Takacs <peter.takacs@koodilab.com>
2018-12-02 01:06:53 +0100

    Added the Vue skeleton

---
+++ resources/ts/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app"></div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class App extends Vue {};
+</script>
