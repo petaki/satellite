6600d2f9487fffe6df7a8009cd7bd6554201e789
Peter Takacs <peter.takacs@koodilab.com>
2018-12-03 21:47:08 +0100

    Added the base styles

--- resources/ts/components/Sidebar.vue
+++ resources/ts/components/Sidebar.vue
@@ -1,5 +1,38 @@
 <template>
-    <div class="w-64 bg-blue-darker h-screen"></div>
+    <div class="w-64 bg-indigo-darker min-h-screen">
+        <ul class="list-reset font-sans font-light mt-6">
+            <li>
+                <a class="btn-sidebar active" href="#">
+                    <i class="fas fa-microchip"></i>
+                    CPU
+                </a>
+            </li>
+            <li>
+                <a class="btn-sidebar" href="#">
+                    <i class="fas fa-memory"></i>
+                    Memory
+                </a>
+            </li>
+            <li>
+                <a class="btn-sidebar" href="#">
+                    <i class="fas fa-hdd"></i>
+                    Drive
+                    <small>
+                        /mnt/hdd0/documents
+                    </small>
+                </a>
+            </li>
+            <li>
+                <a class="btn-sidebar" href="#">
+                    <i class="fas fa-hdd"></i>
+                    Drive
+                    <small>
+                        /mnt/hdd1/pictures
+                    </small>
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
--- tailwind.js
+++ tailwind.js
@@ -104,9 +104,9 @@ let colors = {
   'blue-lighter': '#bcdefa',
   'blue-lightest': '#eff8ff',
 
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
+  'indigo-darkest': '#0d1122',
+  'indigo-darker': '#121930',
+  'indigo-dark': '#3e4b71',
   'indigo': '#6574cd',
   'indigo-light': '#7886d7',
   'indigo-lighter': '#b2b7ff',
