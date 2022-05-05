40cedbfc8bec098c70b8386cbf1ae8f9aee69b74
Peter Takacs <peter.takacs@koodilab.com>
2022-05-05 23:15:52 +0200

    Fixed the layout

--- resources/js/common/Layout.vue
+++ resources/js/common/Layout.vue
@@ -1,80 +1,78 @@
 <template>
-    <section class="flex">
-        <div v-show="isSidebarOpen"
-             class="bg-black bg-opacity-50 fixed inset-0 z-10 md:hidden"
-             @click.prevent="isSidebarOpen = false"></div>
-        <!-- eslint-disable max-len -->
-        <div class="fixed inset-y-0 left-0 bg-gray-800 w-60 z-20 transform transition-transform md:static md:translate-x-0"
-             :class="{'translate-x-0': isSidebarOpen, '-translate-x-full': !isSidebarOpen}">
-            <div class="flex h-20 bg-gray-900">
-                <inertia-link class="flex items-center m-auto text-white text-xl"
-                              href="/">
-                    <paper-airplane-icon class="h-7 w-7 mr-2" />
-                    <span>
-                        {{ $page.props.title }}
-                    </span>
-                </inertia-link>
-            </div>
-            <div class="sidebar overflow-y-auto">
-                <sidebar-title>
-                    Performance
-                </sidebar-title>
-                <sidebar-link :is-active="!!$page.props.isCpuActive"
-                              href="/">
-                    <chip-icon class="h-5 w-5 mr-2" />
-                    <span>
-                        CPU
-                    </span>
-                </sidebar-link>
-                <sidebar-link :is-active="!!$page.props.isMemoryActive"
-                              href="/memory">
-                    <duplicate-icon class="h-5 w-5 mr-2" />
-                    <span>
-                        Memory
-                    </span>
-                </sidebar-link>
-                <sidebar-title>
-                    Disks
-                </sidebar-title>
-                <div v-if="!$page.props.diskPaths" class="px-5 py-3.5 text-slate-500">
-                    No data found.
-                </div>
-                <sidebar-link v-for="diskPath in $page.props.diskPaths"
-                              :key="diskPath"
-                              :is-active="$page.props.diskPath === diskPath"
-                              :href="`/disk?path=${diskPath}`">
-                    <database-icon class="h-5 w-5 mr-2" />
-                    <div>
-                        Disk
-                        <div class="text-xs">
-                            {{ diskPath }}
-                        </div>
-                    </div>
-                </sidebar-link>
-            </div>
-            <div class="flex h-20 bg-slate-700 bg-opacity-40 text-sm text-slate-300">
-                <span class="m-auto">
-                    &copy; {{ year }}
-                    <span class="text-cyan-500">
-                        {{ $page.props.title }}
-                    </span>
+    <div v-show="isSidebarOpen"
+         class="bg-black bg-opacity-50 fixed inset-0 z-10 md:hidden"
+         @click.prevent="isSidebarOpen = false"></div>
+    <!-- eslint-disable max-len -->
+    <div class="fixed inset-y-0 left-0 bg-gray-800 w-60 z-20 transform transition-transform md:translate-x-0"
+         :class="{'translate-x-0': isSidebarOpen, '-translate-x-full': !isSidebarOpen}">
+        <div class="flex h-20 bg-gray-900">
+            <inertia-link class="flex items-center m-auto text-white text-xl"
+                          href="/">
+                <paper-airplane-icon class="h-7 w-7 mr-2" />
+                <span>
+                    {{ $page.props.title }}
                 </span>
+            </inertia-link>
+        </div>
+        <div class="sidebar overflow-y-auto">
+            <sidebar-title>
+                Performance
+            </sidebar-title>
+            <sidebar-link :is-active="!!$page.props.isCpuActive"
+                          href="/">
+                <chip-icon class="h-5 w-5 mr-2" />
+                <span>
+                    CPU
+                </span>
+            </sidebar-link>
+            <sidebar-link :is-active="!!$page.props.isMemoryActive"
+                          href="/memory">
+                <duplicate-icon class="h-5 w-5 mr-2" />
+                <span>
+                    Memory
+                </span>
+            </sidebar-link>
+            <sidebar-title>
+                Disks
+            </sidebar-title>
+            <div v-if="!$page.props.diskPaths" class="px-5 py-3.5 text-slate-500">
+                No data found.
             </div>
+            <sidebar-link v-for="diskPath in $page.props.diskPaths"
+                          :key="diskPath"
+                          :is-active="$page.props.diskPath === diskPath"
+                          :href="`/disk?path=${diskPath}`">
+                <database-icon class="h-5 w-5 mr-2" />
+                <div>
+                    Disk
+                    <div class="text-xs">
+                        {{ diskPath }}
+                    </div>
+                </div>
+            </sidebar-link>
         </div>
-        <div class="flex-1">
-            <header class="flex items-center bg-white h-20 shadow-sm px-5">
-                <a class="md:hidden"
-                   href="#"
-                   @click.prevent="isSidebarOpen = true">
-                    <menu-icon class="h-6 w-6" />
-                </a>
-            </header>
-            <main class="content overflow-y-auto">
-                <slot></slot>
-            </main>
+        <div class="flex h-20 bg-slate-700 bg-opacity-40 text-sm text-slate-300">
+            <span class="m-auto">
+                &copy; {{ year }}
+                <span class="text-cyan-500">
+                    {{ $page.props.title }}
+                </span>
+            </span>
         </div>
-        <!-- eslint-enable max-len -->
-    </section>
+    </div>
+    <div class="md:ml-60">
+        <header class="flex items-center bg-white h-20 shadow-sm px-5">
+            <a class="md:hidden"
+               href="#"
+               @click.prevent="isSidebarOpen = true">
+                <menu-icon class="h-6 w-6" />
+            </a>
+        </header>
+        <main class="content overflow-y-auto">
+            <slot></slot>
+        </main>
+    </div>
+    <!-- eslint-enable max-len -->
 </template>
 
 <script>
