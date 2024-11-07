5fef5aed7d4e03ae13aaee2334c780cf1f890ba9
Peter Takacs <peter.takacs@koodilab.com>
2024-11-07 19:17:24 +0100

    Added new series types

--- internal/models/series.go
+++ internal/models/series.go
@@ -4,15 +4,86 @@ package models
 type SeriesType string
 
 const (
-	// Day series.
-	Day SeriesType = "day"
+	// Last5Minutes series.
+	Last5Minutes SeriesType = "last_5_minutes"
 
-	// Week series.
-	Week SeriesType = "week"
+	// Last15Minutes series.
+	Last15Minutes SeriesType = "last_15_minutes"
 
-	// Month series.
-	Month SeriesType = "month"
+	// Last30Minutes series.
+	Last30Minutes SeriesType = "last_30_minutes"
+
+	// Last1Hour series.
+	Last1Hour SeriesType = "last_1_hour"
+
+	// Last3Hours series.
+	Last3Hours SeriesType = "last_3_hours"
+
+	// Last6Hours series.
+	Last6Hours SeriesType = "last_6_hours"
+
+	// Last12Hours series.
+	Last12Hours SeriesType = "last_12_hours"
+
+	// Last24Hours series.
+	Last24Hours SeriesType = "last_24_hours"
+
+	// Last2Days series.
+	Last2Days SeriesType = "last_2_days"
+
+	// Last7Days series.
+	Last7Days SeriesType = "last_7_days"
+
+	// Last30Days series.
+	Last30Days SeriesType = "last_30_days"
 )
+
+var SeriesTypes = []map[string]interface{}{
+	{
+		"name":  "Last 5 minutes",
+		"value": Last5Minutes,
+	},
+	{
+		"name":  "Last 15 minutes",
+		"value": Last15Minutes,
+	},
+	{
+		"name":  "Last 30 minutes",
+		"value": Last30Minutes,
+	},
+	{
+		"name":  "Last 1 hour",
+		"value": Last1Hour,
+	},
+	{
+		"name":  "Last 3 hours",
+		"value": Last3Hours,
+	},
+	{
+		"name":  "Last 6 hours",
+		"value": Last6Hours,
+	},
+	{
+		"name":  "Last 12 hours",
+		"value": Last12Hours,
+	},
+	{
+		"name":  "Last 24 hours",
+		"value": Last24Hours,
+	},
+	{
+		"name":  "Last 2 days",
+		"value": Last2Days,
+	},
+	{
+		"name":  "Last 7 days",
+		"value": Last7Days,
+	},
+	{
+		"name":  "Last 30 days",
+		"value": Last30Days,
+	},
+}
 
 // Value type.
 type Value struct {
